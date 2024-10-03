778ad6b37ae9a0ff10ee7c6a096d05442253d483
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-10-03 21:18:03 +0300

    add tests for repositories

---
+++ tests/repositories/database/test_base.py
@@ -0,0 +1,30 @@
+from typing import Iterable, Any
+
+from sqlalchemy.sql import Executable
+
+from src.models.base import Base
+from src.repositories.database.base import SQLAlchemyRepository
+
+
+class TestSQLAlchemyRepository:
+    repository = SQLAlchemyRepository()
+
+    async def test_add(self, obj: Base) -> None:
+        await self.repository.add(obj=obj)
+
+    async def test_merge(self, obj: Base) -> None:
+        await self.repository.merge(obj=obj)
+
+    async def test_execute(self, statement: Executable) -> None:
+        await self.repository.execute(statement=statement)
+
+    async def test_get(self, table: type[Base], primary_key: Any) -> None:
+        response = await self.repository.get(table=table, primary_key=primary_key)
+        assert isinstance(response, Base | None)
+
+    async def test_scalar(self, statement: Executable) -> None:
+        await self.repository.scalar(statement=statement)
+
+    async def test_scalars(self, statement: Executable) -> None:
+        response = await self.repository.scalars(statement=statement)
+        assert isinstance(response, Iterable)
