a0dffb493115908bf81e3168813575c5a0963e1f
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-11-02 20:13:08 +0300

    remove converters

--- src/repositories/database/albums.py
+++ src/repositories/database/albums.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
+from typing import Iterable
 
-from sqlalchemy import select, delete, func
+from sqlalchemy import Executable, select
 
-from src.converters.repositories.database.sqlalchemy import model_to_response_dto, models_to_dto, request_dto_to_model
 from src.dtos.database.albums import (
+    AlbumRequestDTO,
     AlbumResponseDTO,
-    CreateAlbumRequestDTO,
     AlbumsResponseDTO,
-    UpdateAlbumRequestDTO
+    AlbumItemResponseDTO,
 )
 from src.models.albums import Album
 from src.repositories.database.base import SQLAlchemyRepository
@@ -15,41 +15,50 @@ from src.repositories.database.base import SQLAlchemyRepository
 
 @dataclass
 class AlbumRepository(SQLAlchemyRepository):
-    async def create_album(self, album: CreateAlbumRequestDTO) -> int:
-        model = request_dto_to_model(model=Album, request_dto=album)
-        await self.add(model)
-        return model.id
+    async def get_album(self, request: AlbumRequestDTO) -> AlbumResponseDTO:
+        model: Album | None = await self.get(Album, request.id)
+        return AlbumResponseDTO.model_validate(model) if model else None
 
-    async def get_album_by_id(self, album_id: int) -> AlbumResponseDTO | None:
-        model = await self.get(Album, album_id)
-        return model_to_response_dto(response_dto=AlbumResponseDTO, model=model)
+    async def get_albums(self, offset: int = 0, limit: int = 10) -> AlbumsResponseDTO:
+        query: Executable = select(Album).offset(offset).limit(limit).order_by(len(Album.viewers))
+        models: Iterable[Album] = await self.scalars(query)
+        return AlbumsResponseDTO(albums=list(map(
+            lambda album: AlbumItemResponseDTO.model_validate(album),
+            models
+        )))
 
-    async def edit_album(self, album: UpdateAlbumRequestDTO) -> int:
-        model = request_dto_to_model(model=Album, request_dto=album)
-        await self.merge(model)
-        return model.id
-
-    async def get_all_albums(self, offset: int = 0, limit: int = 10) -> AlbumsResponseDTO:
-        query = select(Album).offset(offset).limit(limit).order_by(Album.updated_at.desc())
-        albums = list(await self.scalars(query))
-        return AlbumsResponseDTO(albums=models_to_dto(models=albums, dto=AlbumResponseDTO))
-
-    async def get_albums_count(self) -> int:
-        query = select(func.count(Album.id))
-        return await self.scalar(query)
-
-    async def get_user_albums(self, user_id: int, offset: int = 0, limit: int = 10) -> AlbumsResponseDTO:
-        query = select(Album).filter_by(user_id=user_id).offset(offset).limit(limit).order_by(Album.updated_at.desc())
-        albums = list(await self.scalars(query))
-        return AlbumsResponseDTO(albums=models_to_dto(models=albums, dto=AlbumResponseDTO))
-
-    async def get_user_albums_count(self, user_id: int) -> int:
-        query = select(func.count(Album.id)).filter_by(user_id=user_id)
-        return await self.scalar(query)
-
-    async def delete_album(self, album_id: int, user_id: int) -> None:
-        query = delete(Album).filter_by(id=album_id, user_id=user_id)
-        await self.execute(query)
+    # async def create_album(self, album: CreateAlbumRequestDTO) -> int:
+    #     model = request_dto_to_model(model=Album, request_dto=album)
+    #     await self.add(model)
+    #     return model.id
+    #
+    # async def edit_album(self, album: UpdateAlbumRequestDTO) -> int:
+    #     model = request_dto_to_model(model=Album, request_dto=album)
+    #     await self.merge(model)
+    #     return model.id
+    #
+    # async def get_all_albums(self, offset: int = 0, limit: int = 10) -> AlbumsResponseDTO:
+    #     query = select(Album).offset(offset).limit(limit).order_by(Album.updated_at.desc())
+    #     albums = list(await self.scalars(query))
+    #     return AlbumsResponseDTO(albums=models_to_dto(models=albums, dto=AlbumResponseDTO))
+    #
+    # async def get_albums_count(self) -> int:
+    #     query =
+    #     return await self.scalar(query)
+    #     await self.scalar(select(func.count(Album.id)))
+    #
+    # async def get_user_albums(self, user_id: int, offset: int = 0, limit: int = 10) -> AlbumsResponseDTO:
+    #     query = select(Album).filter_by(user_id=user_id).offset(offset).limit(limit).order_by(Album.updated_at.desc())
+    #     albums = list(await self.scalars(query))
+    #     return AlbumsResponseDTO(albums=models_to_dto(models=albums, dto=AlbumResponseDTO))
+    #
+    # async def get_user_albums_count(self, user_id: int) -> int:
+    #     query = select(func.count(Album.id)).filter_by(user_id=user_id)
+    #     return await self.scalar(query)
+    #
+    # async def delete_album(self, album_id: int, user_id: int) -> None:
+    #     query = delete(Album).filter_by(id=album_id, user_id=user_id)
+    #     await self.execute(query)
 
 
 def init_albums_repository() -> AlbumRepository:
--- tests/repositories/database/test_albums.py
+++ tests/repositories/database/test_albums.py
@@ -2,47 +2,46 @@ import pytest
 
 from src.dtos.database.albums import (
     AlbumResponseDTO,
-    CreateAlbumRequestDTO,
     AlbumsResponseDTO,
-    UpdateAlbumRequestDTO,
+    AlbumRequestDTO,
 )
 from src.repositories.database.albums import AlbumRepository, init_albums_repository
 
 
 class TestAlbumRepository:
     repository: AlbumRepository = init_albums_repository()
-
-    async def test_create_album(self, album: CreateAlbumRequestDTO) -> None:
-        response = await self.repository.create_album(album=album)
-        assert isinstance(response, int)
+    #
+    # async def test_create_album(self, album: CreateAlbumRequestDTO) -> None:
+    #     response = await self.repository.create_album(album=album)
+    #     assert isinstance(response, int)
 
     async def test_get_album_by_id(self, album_id: int) -> None:
-        result = await self.repository.get_album_by_id(album_id=album_id)
+        result = await self.repository.get_album(request=AlbumRequestDTO(id=album_id))
         assert isinstance(result, AlbumResponseDTO | None)
-
-    async def test_edit_album(self, album: UpdateAlbumRequestDTO) -> None:
-        result = await self.repository.edit_album(album=album)
-        assert isinstance(result, int)
+    #
+    # async def test_edit_album(self, album: UpdateAlbumRequestDTO) -> None:
+    #     result = await self.repository.edit_album(album=album)
+    #     assert isinstance(result, int)
 
     @pytest.mark.parametrize("offset,limit", [(i, 10 - i) for i in range(10)])
     async def test_get_all_albums(self, offset: int, limit: int) -> None:
-        response = await self.repository.get_all_albums(offset=offset, limit=limit)
+        response = await self.repository.get_albums(offset=offset, limit=limit)
         assert isinstance(response, AlbumsResponseDTO)
-
-    async def test_get_albums_count(self) -> None:
-        response = await self.repository.get_albums_count()
-        assert isinstance(response, int)
-        assert response >= 0
-
-    @pytest.mark.parametrize("offset,limit", [(i, 10 - i) for i in range(10)])
-    async def test_get_user_albums(self, user_id: int, offset: int, limit: int) -> None:
-        response = await self.repository.get_user_albums(user_id=user_id, offset=offset, limit=limit)
-        assert isinstance(response, AlbumsResponseDTO)
-
-    async def test_get_user_albums_count(self, user_id: int) -> None:
-        response = await self.repository.get_user_albums_count(user_id=user_id)
-        assert isinstance(response, int)
-        assert response >= 0
-
-    async def test_delete_album(self, album_id: int, user_id: int) -> None:
-        await self.repository.delete_album(album_id=album_id, user_id=user_id)
+    #
+    # async def test_get_albums_count(self) -> None:
+    #     response = await self.repository.get_albums_count()
+    #     assert isinstance(response, int)
+    #     assert response >= 0
+    #
+    # @pytest.mark.parametrize("offset,limit", [(i, 10 - i) for i in range(10)])
+    # async def test_get_user_albums(self, user_id: int, offset: int, limit: int) -> None:
+    #     response = await self.repository.get_user_albums(user_id=user_id, offset=offset, limit=limit)
+    #     assert isinstance(response, AlbumsResponseDTO)
+    #
+    # async def test_get_user_albums_count(self, user_id: int) -> None:
+    #     response = await self.repository.get_user_albums_count(user_id=user_id)
+    #     assert isinstance(response, int)
+    #     assert response >= 0
+    #
+    # async def test_delete_album(self, album_id: int, user_id: int) -> None:
+    #     await self.repository.delete_album(album_id=album_id, user_id=user_id)
