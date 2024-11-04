60e064e40c493b245128ea56aef2bc92eec4a1f2
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-11-04 16:51:35 +0300

    update tests

--- tests/repositories/database/test_albums.py
+++ tests/repositories/database/test_albums.py
@@ -17,25 +17,21 @@ from src.dtos.database.base import ItemsRequestDTO
 from src.repositories.database.albums import AlbumRepository, init_albums_repository
 
 
-@pytest.fixture(autouse=True)
-def stub_album_for_create():
-    album = CreateAlbumRequestDTO(title="test", picture_url="test",
-                                  description="test", type="test")
-    return album
-
-
-@pytest.fixture(autouse=True)
-def stub_album_for_update():
-    album = UpdateAlbumRequestDTO(id=1)
-    return album
-
-
 class TestAlbumRepository:
     repository: AlbumRepository = init_albums_repository()
 
+    @pytest.fixture(scope='class')
+    def create_album_request(self):
+        return CreateAlbumRequestDTO(
+            title="test",
+            picture_url="test",
+            description="test",
+            type="test",
+        )
+
     @pytest.mark.dependency()
-    async def test_create_album(self, stub_album_for_create: CreateAlbumRequestDTO, request: pytest.FixtureRequest) -> None:
-        response: CreateAlbumResponseDTO = await self.repository.create_album(album=stub_album_for_create)
+    async def test_create_album(self, create_album_request: CreateAlbumRequestDTO, request: pytest.FixtureRequest) -> None:
+        response: CreateAlbumResponseDTO = await self.repository.create_album(album=create_album_request)
         assert isinstance(response, CreateAlbumResponseDTO)
         request.node.album_id = response.id
 
@@ -43,14 +39,18 @@ class TestAlbumRepository:
     async def album_id(self, request: pytest.FixtureRequest) -> int:
         return request.node.album_id
 
+    @pytest.fixture(scope='class')
+    def update_album_request(self, album_id: int):
+        return UpdateAlbumRequestDTO(id=album_id)
+
     @pytest.mark.dependency(depends=['TestAlbumRepository::test_create_album'])
     async def test_get_album(self, album_id: int) -> None:
         response: AlbumResponseDTO | None = await self.repository.get_album(request=AlbumRequestDTO(id=album_id))
         assert isinstance(response, AlbumResponseDTO | None)
 
-    @pytest.mark.dependency(depends=['TestAlbumRepository::test_create_album'])
-    async def test_update_album(self, stub_album_for_update: UpdateAlbumRequestDTO, request: pytest.FixtureRequest) -> None:
-        response: UpdateAlbumResponseDTO = await self.repository.update_album(album=stub_album_for_update)
+    @pytest.mark.dependency(depends=['TestAlbumRepository::test_create_album', 'TestAlbumRepository::test_get_album'])
+    async def test_update_album(self, update_album_request: UpdateAlbumRequestDTO, request: pytest.FixtureRequest) -> None:
+        response: UpdateAlbumResponseDTO = await self.repository.update_album(album=update_album_request)
         assert isinstance(response, UpdateAlbumResponseDTO)
         request.node.album_id = response.id
 
