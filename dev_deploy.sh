d77f565245b234accd698114d2cfcb09213df0c3
usmskolyadin <koladinmaksim246@gmail.com>
2024-11-15 23:19:49 +0300

    update sh files

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -10,18 +10,8 @@ fi
 
 cd "$DIR" || exit
 
-git pull origin master
+git pull origin dev
 
 make start
-
-# Сборка проекта (если это необходимо)
-npm run build
-
-# Перезапуск сервиса (например, через PM2 или systemd)
-# Если ты используешь PM2
-pm2 restart your-app-name
-
-# Или если через systemd
-# sudo systemctl restart your-service.service
 
 echo "Deployment completed successfully! 🎉"
