3ca8ce900a8a95a8fee05e30edcdae416e2ff789
IlyaBulatau <ilyabulatau@gmail.com>
2024-12-08 19:29:30 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,22 @@
+import asyncio
+from asyncio.events import AbstractEventLoop
+from typing import Any, Generator
+
 import pytest
 from fastapi.testclient import TestClient
+
 from src.api.app import app
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def client() -> TestClient:
     return TestClient(app=app)
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Generator[AbstractEventLoop, Any, None]:
+    loop = asyncio.new_event_loop()
+    try:
+        yield loop
+    finally:
+        loop.close()
