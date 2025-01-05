19d5b544c9e6be3be5268997dee61c56124abe2a
Ilya Kuznetsov <ilkztsff@gmail.com>
2025-01-05 04:44:46 +0300

    add schemas

---
+++ src/domain/music/albums/api/schemas.py
@@ -0,0 +1,159 @@
+from abc import ABC
+from datetime import date, datetime
+from typing import Literal, Sequence
+
+
+class BaseSUser(ABC):
+    id: int
+    username: str
+    description: str | None = None
+    email: str
+    password: str
+    picture_url: str | None = None
+    access_level: Literal['user', 'admin', 'superuser'] = 'user'
+    telegram_id: int | None = None
+    premium_level: Literal['none', 'bot', 'full'] = 'none'
+
+    is_active: bool
+    is_adult: bool
+    is_verified: bool
+
+    created_at: date
+    updated_at: datetime
+
+
+class BaseSArtist(ABC):
+    id: int
+    username: str
+    description: str | None = None
+    picture_url: str | None = None
+    user_id: int
+
+
+class BaseSTrack(ABC):
+    id: int
+    title: str
+    description: str | None
+    picture_url: str | None
+    file_url: str
+    views: int
+    likes: int
+
+    created_at: date
+    updated_at: datetime
+
+
+class BaseSItemsRequest(ABC):
+    start: int = 1
+    size: int = 10
+
+
+class BaseSAlbumRequest(ABC):
+    album_id: int
+    user_id: int
+
+
+class BaseSAlbumResponse(ABC):
+    id: int
+    title: str
+    picture_url: str | None
+    description: str | None
+    type: Literal['album', 'single']
+    views: int
+    likes: int
+
+    created_at: date
+    updated_at: datetime
+
+    artists: Sequence[BaseSArtist]
+    tracks: Sequence[BaseSTrack]
+    tags: Sequence[str]
+
+
+class BaseSAlbumItemResponse(ABC):
+    id: int
+    title: str
+    picture_url: str | None
+    description: str | None
+    views: int
+    likes: int
+    type: Literal['album', 'single']
+
+    created_at: date
+    updated_at: datetime
+
+
+class BaseSPopularAlbumsRequest(ABC):
+    user_id: int
+
+
+class BaseSPopularAlbumsResponse(ABC):
+    total: int
+    page: int
+    has_next: bool
+    has_previous: bool
+    size: int
+    items: Sequence[BaseSAlbumItemResponse]
+
+
+class BaseSCountAlbumsResponse(ABC):
+    amount: int
+
+
+class BaseSArtistAlbumsRequest(ABC):
+    artist_id: int
+
+
+class BaseSArtistAlbumsResponse(ABC):
+    total: int
+    page: int
+    has_next: bool
+    has_previous: bool
+    size: int
+    items: Sequence[BaseSAlbumItemResponse]
+
+
+class BaseSLikeAlbumRequest(ABC):
+    album_id: int
+    user_id: int
+
+
+class BaseSUnlikeAlbumRequest(ABC):
+    album_id: int
+    user_id: int
+
+
+class BaseSUpdateAlbumCoverRequest(ABC):
+    album_id: int
+    user_id: int
+    data: bytes
+
+
+class BaseSCreateAlbumRequest(ABC):
+    title: str
+    user_id: int
+    description: str | None
+    tags: Sequence[str]
+
+
+class BaseSCreateAlbumResponse(ABC):
+    id: int
+
+
+class BaseSUpdateAlbumRequest(ABC):
+    id: int
+    title: str | None = None
+    picture_url: str | None = None
+    description: str | None = None
+
+    artists_ids: Sequence[int] | None = None
+    tracks_ids: Sequence[int] | None = None
+    tags: Sequence[str] | None = None
+
+
+class BaseSUpdateAlbumResponse(ABC):
+    id: int
+
+
+class BaseSDeleteAlbumRequest(ABC):
+    album_id: int
---
+++ src/presentation/albums/schemas.py
@@ -0,0 +1,183 @@
+from datetime import date, datetime
+from typing import Literal
+
+from pydantic import BaseModel, EmailStr
+
+from src.domain.music.albums.api.schemas import (
+    BaseSUser,
+    BaseSArtist,
+    BaseSTrack,
+    BaseSItemsRequest,
+    BaseSAlbumRequest,
+    BaseSAlbumResponse,
+    BaseSAlbumItemResponse,
+    BaseSPopularAlbumsRequest,
+    BaseSPopularAlbumsResponse,
+    BaseSCountAlbumsResponse,
+    BaseSArtistAlbumsRequest,
+    BaseSArtistAlbumsResponse,
+    BaseSLikeAlbumRequest,
+    BaseSUnlikeAlbumRequest,
+    BaseSUpdateAlbumCoverRequest,
+    BaseSCreateAlbumRequest,
+    BaseSCreateAlbumResponse,
+    BaseSUpdateAlbumRequest,
+    BaseSUpdateAlbumResponse,
+    BaseSDeleteAlbumRequest,
+)
+
+
+class SUser(BaseSUser, BaseModel):
+    id: int
+    username: str
+    description: str | None = None
+    email: EmailStr
+    password: str
+    picture_url: str | None = None
+    access_level: Literal['user', 'admin', 'superuser'] = 'user'
+    telegram_id: int | None = None
+    premium_level: Literal['none', 'bot', 'full'] = 'none'
+
+    is_active: bool
+    is_adult: bool
+    is_verified: bool
+
+    created_at: date
+    updated_at: datetime
+
+
+class SArtist(BaseSArtist, BaseModel):
+    id: int
+    username: str
+    description: str | None = None
+    picture_url: str | None = None
+    user_id: int
+
+
+class STrack(BaseSTrack, BaseModel):
+    id: int
+    title: str
+    description: str | None
+    picture_url: str | None
+    file_url: str
+    views: int
+    likes: int
+
+    created_at: date
+    updated_at: datetime
+
+
+class SItemsRequest(BaseSItemsRequest, BaseModel):
+    start: int = 1
+    size: int = 10
+
+
+class SAlbumRequest(BaseSAlbumRequest, BaseModel):
+    album_id: int
+    user_id: int
+
+
+class SAlbumResponse(BaseSAlbumResponse, BaseModel):
+    id: int
+    title: str
+    picture_url: str | None
+    description: str | None
+    type: Literal['album', 'single']
+    views: int
+    likes: int
+
+    created_at: date
+    updated_at: datetime
+
+    artists: list[SArtist]
+    tracks: list[STrack]
+    tags: list[str]
+
+
+class SAlbumItemResponse(BaseSAlbumItemResponse, BaseModel):
+    id: int
+    title: str
+    picture_url: str | None
+    description: str | None
+    views: int
+    likes: int
+    type: Literal['album', 'single']
+
+    created_at: date
+    updated_at: datetime
+
+
+class SPopularAlbumsRequest(BaseSPopularAlbumsRequest, BaseModel):
+    user_id: int
+
+
+class SPopularAlbumsResponse(BaseSPopularAlbumsResponse, BaseModel):
+    total: int
+    page: int
+    has_next: bool
+    has_previous: bool
+    size: int
+    items: list[SAlbumItemResponse]
+
+
+class SCountAlbumsResponse(BaseSCountAlbumsResponse, BaseModel):
+    amount: int
+
+
+class SArtistAlbumsRequest(BaseSArtistAlbumsRequest, BaseModel):
+    artist_id: int
+
+
+class SArtistAlbumsResponse(BaseSArtistAlbumsResponse, BaseModel):
+    total: int
+    page: int
+    has_next: bool
+    has_previous: bool
+    size: int
+    items: list[SAlbumItemResponse]
+
+
+class SLikeAlbumRequest(BaseSLikeAlbumRequest, BaseModel):
+    album_id: int
+    user_id: int
+
+
+class SUnlikeAlbumRequest(BaseSUnlikeAlbumRequest, BaseModel):
+    album_id: int
+    user_id: int
+
+
+class SUpdateAlbumCoverRequest(BaseSUpdateAlbumCoverRequest, BaseModel):
+    album_id: int
+    user_id: int
+    data: bytes
+
+
+class SCreateAlbumRequest(BaseSCreateAlbumRequest, BaseModel):
+    title: str
+    user_id: int
+    description: str | None
+    tags: list[str]
+
+
+class SCreateAlbumResponse(BaseSCreateAlbumResponse, BaseModel):
+    id: int
+
+
+class SUpdateAlbumRequest(BaseSUpdateAlbumRequest, BaseModel):
+    id: int
+    title: str | None = None
+    picture_url: str | None = None
+    description: str | None = None
+
+    artists_ids: list[int] | None = None
+    tracks_ids: list[int] | None = None
+    tags: list[str] | None = None
+
+
+class SUpdateAlbumResponse(BaseSUpdateAlbumResponse, BaseModel):
+    id: int
+
+
+class SDeleteAlbumRequest(BaseSDeleteAlbumRequest, BaseModel):
+    album_id: int
